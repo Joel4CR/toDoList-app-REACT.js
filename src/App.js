9e6b9f5a0f75797bc86f8dd38517d6c36242a491
Joel CR <joel4cr@gmail.com>
2021-11-28 01:41:38 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+const[item,setItem]=useState([])
+const[saveItem,setSaveItem]=useState("")
+let key=1;
+
+function handlerNewItem(event) {
+   setSaveItem (event.target.value)
+}
+
+function onclick() {
+  setItem(prevValue=>[...prevValue,saveItem ])
+  setSaveItem("");
+  key+=key;
+}
+
+
+
+
+
   return(
   <div className="container">
     <div className="heading"> 
       <h1>To-Do List</h1>
     </div>
     <div className="form">
-      <input type="text" />
-      <button><span>Add</span></button>
+      <input type="text" onChange={handlerNewItem} value={saveItem}/>
+      <button onClick={onclick}><span>Add</span></button>
     </div>
     <div>
       <ul>
-        <li>Item</li>
+        {item.map((newItem,key)=>{
+        return(
+        <li key={key}>{newItem}</li>
+          )
+
+        })
+        }
       </ul>
     </div>
   </div>
